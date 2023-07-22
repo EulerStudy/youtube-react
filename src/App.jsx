61e6334ade0f1c0d8249e0d745329bd3e39c018a
Euler Edu <eulerlab@naver.com>
2023-07-22 16:49:10 +0900

    react-youtube

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+/*
+/ <Videos> popular
+/videos <Videos> popular 
+/videos/query <Videos> search
+/videos/watch/id <VideoDetail>
+*/
+
+import { Outlet } from "react-router-dom";
+import SearchHeader from "./components/SearchHeader";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { YoutubeApiProvider } from "./context/YoutubeApiContext";
+
+function App() {
+  const queryClient = new QueryClient()
+
+  return (
+    <>
+      <SearchHeader />
+      <YoutubeApiProvider>
+        <QueryClientProvider client={queryClient} >
+          <Outlet />
+        </QueryClientProvider>
+      </YoutubeApiProvider>
+    </>
+  );
+}
+
+export default App;
